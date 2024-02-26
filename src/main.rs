d6c02937021281d09a7f1902485abe7360ced176
Shiny Mimikyu <mrtoniliu@gmail.com>
2024-02-26 22:14:53 +0800

    Variable, Const, Static

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Static VARIABLE
+static MY_STATIC: i32 = 10;
+static mut MY_MUT_STATIC: i32 = 11;
+
 fn main() {
     /*
     Immutable var
@@ -6,13 +10,13 @@ fn main() {
     let _nice_count = 100;
 
     /* mutable var */
-    let mut count = 3;
-    count = 4;
+    let mut _count = 3; // unused warning can be dismissed by prefix '_'
+    _count = 4;
 
     // Shadowing
     let x = 5;
     {
-        // annonumous naming space
+        // anonymous naming space
         let x: i64 = 10;
         println!("Inner x: {}", x);
         // inner x will be destroyed
@@ -23,5 +27,20 @@ fn main() {
     println!("New x: {x}");
 
     // Constant
-    const _THIS_IS_CONSTANT: i64 = 500;
+    const SECOND_HOUR: usize = 3600;
+    const SECOND_DAY: usize = 24 * SECOND_HOUR;
+
+    {
+        const SE: usize = 1000;
+        println!("SE: {SE}");
+    }
+
+    // println!("{SE}"); // ERROR
+    println!("Seconds per day: {}", SECOND_DAY);
+    println!("My static, {}", MY_STATIC);
+    unsafe {
+        MY_MUT_STATIC = 31;
+        println!("MY mutable static, {MY_MUT_STATIC}");
+    }
+    // println!("MY mutable static, {MY_MUT_STATIC}"); // ERROR
 }
